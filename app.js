4f75d7667659713532f2ebf09c2449b8746ee222
Arjun Santhosh <36653574+ArjunSanthoshpnr@users.noreply.github.com>
2021-01-25 01:15:52 +0530

    Updated app.js with local storage feature.
    1. localStorage functionality is used here to store tasks within the browser. 
    2. The tasks are saved during the reload or closing of the page and restored while loading the page. 
    3. Multiple sessions may not work properly for this app.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var taskList = document.querySelectorAll('.task');
 
-
-var getTaskList = function (list) {
+var addCloseBtn = function (list) {
     var i;
     for(i = 0; i<list.length; i++) {
         var span = document.createElement("span");
@@ -9,7 +8,6 @@ var getTaskList = function (list) {
         span.appendChild(text);
         list[i].appendChild(span);
     }
-
 }
 
 var closeTaskList = function(list) {
@@ -34,9 +32,12 @@ var strikeTaskList = function(list) {
         }
     }
 }
-var addTask = function() {
-    var text = document.getElementById("addInput").value;
-    console.log(text);
+var readTask = function() {
+    var text = document.getElementById('addInput').value;
+    addTask(text);
+}
+var addTask = function(inputText='', strike='none') {
+    var text = inputText;
     var div = document.createElement("div");
     var p = document.createElement("p");
     var t = document.createTextNode(text);
@@ -47,12 +48,50 @@ var addTask = function() {
         alert("Empty task!");
     }
     else {
+        if(strike == '' || strike == 'none')
+            p.style.textDecoration = 'none';
+        else
+            p.style.textDecoration = 'line-through';
+
         document.getElementById("block").appendChild(div);
+            
     }
     document.getElementById("addInput").value = "";
-    getTaskList([div]);
+    addCloseBtn([div]);
     closeTaskList([div]);
     strikeTaskList([div]);
+}
+
+var storeTask = function () {
+    var list = document.querySelectorAll('.task');
+    var text, i, obj=[];
+    for(i = 0; i<list.length; i++) {
+        if(list[i].style.display !== 'none'){
+            text = list[i].children[0].innerText;
+            decoration = list[i].children[0].style.textDecoration;
+            obj.push({task: text, strike: decoration});
+        }
+    }
+    var stringObject = JSON.stringify(obj);
+    if (typeof(Storage) !== "undefined") {
+        localStorage.setItem('taskArray', stringObject);
+    }
+    else{
+        console.log('does not support local storage.');
+    }  
+}
+
+var loadTask = function () {
+    if (typeof(Storage) !== "undefined") {
+        var text = localStorage.getItem('taskArray');
+        var taskArray = JSON.parse(text);
+    }
+    else {
+        console.log('does not support local storage.');
+    }
+    for(i in taskArray){
+        addTask(taskArray[i].task, taskArray[i].strike);
+    }
 }
 
 var input = document.getElementById("addInput");
@@ -62,6 +101,10 @@ input.addEventListener("keyup", function(event) {
         document.getElementById("add").click();
     }
 });
-getTaskList(taskList);
-closeTaskList(taskList);
-strikeTaskList(taskList);
+loadTask();
+
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+    e.returnValue = '';
+    storeTask();
+});
